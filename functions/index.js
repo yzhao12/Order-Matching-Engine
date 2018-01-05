93cef77d19c205d7ea2635f5c42e1b6931a085ab
yzhao12 <yangzhao12@gmail.com>
2018-01-04 23:08:59 -0500

    transaction workflow: scanning the orderbook, and working on adding shares to portfolio for buyer

--- functions/index.js
+++ functions/index.js
@@ -20,9 +20,16 @@ exports.matchOrders = functions.database.ref('/Orders/{ticker}').onWrite(event =
 
   for(var i = 0; i < buyBook.length && i < sellBook.length; i++) {
     if(buyBook[i].shares == sellBook[i].shares && buyBook[i].price >= sellBook[i].price) {
-      buyUser = functions.database.ref('/Users/' + buyBook[i].userid);
-      sellUser = functions.database.ref('/Users/' + sellBook[i].userid);
+      var buyUser = functions.database.ref('/Users/' + buyBook[i].userid);
+      var sellUser = functions.database.ref('/Users/' + sellBook[i].userid);
+
+      buyUser.once('value').then(function(snapshot) {
+        buyUser = snapshot.val();
+      });
+      sellUser.once('value').then(function(snapshot)) {
+        sellUser = snapshot.val();
+      });
 
       //adding shares to portfolio for buyer
-      portfolio = buyUser.
+      portfolio = buyUser.userPortfolio;
 });
